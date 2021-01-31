dfb80a3a36b8fd4e36835693338ebf0a373a82ce
KiLJ4EdeN <parsa592323@gmail.com>
2021-01-31 15:00:28 +0330

    remove dep to cv2

--- model_api.py
+++ model_api.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, File, UploadFile
 from tensorflow.keras.models import load_model
-import cv2
 import numpy as np
+from PIL import Image
+from io import BytesIO
+
 # to test the service using curl:
 # curl -X POST "http://127.0.0.1:8000/predict" -H  "accept: application/json" -H  "Content-Type: multipart/form-data" -F "image=@mnist_sample.jpg;type=image/jpeg"
 
@@ -16,9 +18,7 @@ model = load_model('mnist_model.h5')
 # define a form with a multipart input, which will be the image in this case.
 async def predict(image: UploadFile = File(...)):
     contents = await image.read()
-    with open(f'{image.filename}', 'wb') as f:
-        f.write(contents)
-    loaded_image = cv2.imread(image.filename, cv2.IMREAD_GRAYSCALE)
+    loaded_image = Image.open(BytesIO(contents))
     loaded_image = np.expand_dims(loaded_image, axis=0)
     prediction = np.argmax(model.predict(loaded_image))
     return {"label": str(prediction)}
